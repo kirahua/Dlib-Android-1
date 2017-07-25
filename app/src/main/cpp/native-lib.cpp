9ccb73952393d70a0a89b1d88ebc2ba7cf0937e4
chengli <orient2008.cl@hotmail.com>
2017-07-26 00:12:32 +0800

    Use a picture to test function detect face by Dlib.

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -1,13 +1,16 @@
 #include <jni.h>
 #include <string>
+#include <dlib/external/libjpeg/jpeglib.h>
 #include "dlib/image_processing/frontal_face_detector.h"
 #include "dlib/image_io.h"
+#include <iostream>
+#include <opencv/cv.h>
+#include <opencv2/opencv.hpp>
 #include <android/log.h>
-#include <include/opencv2/core.hpp>
+#include <dlib/opencv/cv_image.h>
 
 #define LOGW(...)  __android_log_print(ANDROID_LOG_WARN,"native-lib",__VA_ARGS__)
 using namespace dlib;
-using namespace cv;
 
 extern "C"
 JNIEXPORT jstring JNICALL
@@ -18,19 +21,19 @@ Java_com_orange_opencv_MainActivity_stringFromJNI(JNIEnv *env, jobject) {
 
 extern "C"
 JNIEXPORT void JNICALL
-Java_com_orange_opencv_MainActivity_loadJpeg(JNIEnv *env, jobject instance, jstring path_) {
-    clock_t start, finish;
-    start = clock();
-    const char *path = env->GetStringUTFChars(path_, NULL);
+Java_com_orange_opencv_MainActivity_loadJpeg(JNIEnv *env, jobject instance, jlong native_addr) {
+
+
     frontal_face_detector detector = get_frontal_face_detector();
-    array2d<unsigned char> img;
-    load_image(img, path);
-    pyramid_up(img);
+    cv::Mat *src = &(*((cv::Mat *) native_addr));
+    cv::Mat temp;
+    cv::cvtColor(*src, temp, CV_BGR2GRAY);
+    cv_image<unsigned char> cimg(temp);
 
-    std::vector<rectangle> dets = detector(img,0);
-    finish = clock();
-    LOGW(" spend = %d", ((finish - start)/ CLOCKS_PER_SEC));
-    env->ReleaseStringUTFChars(path_, path);
+    std::vector<rectangle> dets = detector(cimg, 0);
+    for (unsigned long j = 0; j < dets.size(); ++j) {
+        cv::Rect rect =  cv::Rect(dets[j].left(), dets[j].top(), dets[j].width(), dets[j].height());
+        cv::rectangle(*src, rect, cv::Scalar(255, 255, 255));
+    }
     LOGW(" dets size = %d", dets.size());
-
 }
--- app/src/main/java/com/orange/opencv/MainActivity.java
+++ app/src/main/java/com/orange/opencv/MainActivity.java
@@ -1,10 +1,18 @@
 package com.orange.opencv;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
+
+import org.opencv.android.Utils;
+import org.opencv.core.CvType;
+import org.opencv.core.Mat;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,21 +22,27 @@ public class MainActivity extends AppCompatActivity {
         System.loadLibrary("native-lib");
     }
 
-    private static final String ROOT = Environment.getExternalStorageDirectory().getAbsolutePath();
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         TextView tv = (TextView) findViewById(R.id.sample_text);
+        ImageView iv = (ImageView) findViewById(R.id.iv);
         long start = System.currentTimeMillis();
-        loadJpeg(ROOT + "/lena.jpg");
+
+        Bitmap bitmap = BitmapFactory.decodeResource(this.getResources(), R.drawable.lena);
+        Mat mat = new Mat(bitmap.getWidth(), bitmap.getHeight(), CvType.CV_8UC1);
+        Utils.bitmapToMat(bitmap, mat);
+        loadJpeg(mat.nativeObj);
+        Utils.matToBitmap(mat, bitmap);
+        iv.setImageBitmap(bitmap);
+
         Log.w(TAG, "SPEND :" + (System.currentTimeMillis() - start));
         tv.setText(stringFromJNI());
     }
 
     public native String stringFromJNI();
 
-    public native void loadJpeg(String path);
+    public native void loadJpeg(long nativeObj);
 }
